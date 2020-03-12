38061f02f32c076b6c96c9fc3479307078e792f6
Rainan Miranda de Jesus <jesus.rainan@gmail.com>
2020-03-12 14:10:19 -0300

    Show and list methods created in book controller

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -2,21 +2,49 @@ const { book } = require('../models/')
 
 module.exports = {
     async store(request, response){
-        const {bk_title, bk_subtitle, bk_author, bk_category} = request.body
+        const {bk_title, bk_subtitle, bk_author, bk_book} = request.body
 
-        if (typeof(bk_title)!=='string'||typeof(bk_subtitle)!=='string'||typeof(bk_author)!=='number'||typeof(bk_category)!=='number'){
+        if (typeof(bk_title)!=='string'||typeof(bk_subtitle)!=='string'||typeof(bk_author)!=='number'||typeof(bk_book)!=='number'){
             return response.status(400).json({success: false, messageError: `You must specify all the book parameters!`}) 
         }
 
         try {
-            Book = await book.create({bk_title, bk_subtitle, bk_author, bk_category})
+            Book = await book.create({bk_title, bk_subtitle, bk_author, bk_book})
 
             console.log("Book created successfully!")
 
             return response.json(Book)
         } catch (error) {
             console.log(`Error during book creation: ${error}`)
-            return response.json({success: false, messageError: "Error during book creation"})
+            return response.status(500).json({success: false, messageError: "Error during book creation"})
+        }
+    },
+    async show(request, response){
+        const {bk_id} = request.params
+
+        try {
+            const Book = await book.findByPk(bk_id)
+
+            if(Book!==null){
+                console.log("Request successfully found data!")
+
+                return response.json(Book)
+            }
+        } catch (error) {
+            console.log(`Error during data search: ${error}`)
+            return response.status(500).json({success: false, messageError: "Error during data search"})
+        }
+    },
+    async list(request, response){
+        try {
+            const Books = await book.findAll()
+
+            console.log("Request successfully found data!")
+
+            return response.json(Books)
+        } catch (error) {
+            console.log(`Error during data search: ${error}`)
+            return response.status(500).json({success: false, messageError: "Error during data search"})
         }
     }
 }
