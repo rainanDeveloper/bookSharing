b57168add58eea506750a71f021cf45f8c8152c7
Rainan Miranda de Jesus <jesus.rainan@gmail.com>
2020-03-31 18:36:07 -0300

    Layout improved

--- frontend/src/components/Login/index.js
+++ frontend/src/components/Login/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Link} from 'react-router-dom'
 import './styles.css'
 import logo from '../../logo.svg'
 
 function Login() {
     return (
-      <div className="LoginForm">
-        <h1 className="projectTitle"><img src={logo} alt=""/><p>Book<span>Sharing</span></p></h1>
-        <form>
-          <input placeholder="Login"/>
-          <input type="password" placeholder="Senha"/>
-          <p>Não tem uma conta? <a href="/SignUp">Cadastre-se</a></p>
-          <button className="loginButton">Login</button>
-        </form>
+      <div className="Login">
+        <div className="LoginForm">
+          <h1 className="projectTitle"><img src={logo} alt=""/><p>Book<span>Sharing</span></p></h1>
+          <form>
+            <input placeholder="Login"/>
+            <input type="password" placeholder="Senha"/>
+            <p>Não tem uma conta? <Link to="/SignUp">Cadastre-se</Link></p>
+            <button className="loginButton">Login</button>
+          </form>
+        </div>
       </div>
     );
   }
--- frontend/src/components/SignUp/index.js
+++ frontend/src/components/SignUp/index.js
@@ -1,19 +1,29 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 import ReactDOM from 'react-dom';
 import './styles.css'
 import logo from '../../logo.svg'
 
 function Login() {
     return (
-      <div className="LoginForm">
-        <h1 className="projectTitle"><img src={logo} alt=""/><p>Book<span>Sharing</span></p></h1>
-        <form>
-          <input placeholder="E-mail"/>
-          <input placeholder="Login"/>
-          <input type="password" placeholder="Senha"/>
-          <p>Já possui uma conta? <a href="/">Login</a></p>
-          <button className="loginButton">Login</button>
-        </form>
+      <div className="SignUp">
+        <section>
+          <h1 className="projectTitle"><img src={logo} alt=""/><p>Book<span>Sharing</span></p></h1>
+        </section>
+        <div className="LoginForm">
+          <form>
+            <input placeholder="E-mail"/>
+            <input placeholder="Login"/>
+            <input type="password" placeholder="Senha"/>
+            <input placeholder="Nome"/>
+            <div className="input-group">
+              <input placeholder="CFP"/>
+              <input type="date"/>
+            </div>
+            <p>Já possui uma conta? <Link to="/">Login</Link></p>
+            <button className="loginButton">Login</button>
+          </form>
+        </div>
       </div>
     );
   }
