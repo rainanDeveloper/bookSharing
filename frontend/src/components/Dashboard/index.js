8911b89c7918c1828a66270142b92509bec1bb45
Rainan Miranda de Jesus <jesus.rainan@gmail.com>
2020-04-01 18:22:41 -0300

    Dashboard initiated

--- frontend/src/components/Dashboard/index.js
+++ frontend/src/components/Dashboard/index.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-
+import {Link, useHistory} from 'react-router-dom'
 import api from '../../services/api'
 import './styles.css'
 import logo from '../../logo.svg'
 
 function Dashboard(){
+
+    const accessToken = localStorage.getItem('access-token')
+
     return (
-        <h1>DashBoard</h1>
+        <div className="DashBoard">
+            <header>
+                <h1 className="projectTitle"><img src={logo} alt=""/><p>Book<span>Sharing</span></p></h1>
+                <div className="menu">
+                    <button></button>
+                </div>
+            </header>
+        </div>
     )
 }
 
