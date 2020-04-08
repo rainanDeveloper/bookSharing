2786edc6cced5339768a22f130712f89d415e202
Rainan Miranda de Jesus <jesus.rainan@gmail.com>
2020-04-07 21:05:04 -0300

    Added logout method

--- frontend/src/components/Dashboard/index.js
+++ frontend/src/components/Dashboard/index.js
@@ -1,16 +1,21 @@
 import React, {useState, useEffect} from 'react'
 import {Link, useHistory} from 'react-router-dom'
-import {FiSettings, FiTrash2} from 'react-icons/fi'
+import {FiSettings, FiTrash2,FiPlus, FiLogOut} from 'react-icons/fi'
 import api from '../../services/api'
 import './styles.css'
 import logo from '../../logo.svg'
 
 function Dashboard(){
     const [requests, setRequests] = useState([])
+    const [modelContent, setModalContent] = useState(<div/>)
 
     const history = useHistory()
 
     const accessToken = localStorage.getItem('access-token')
+
+    if(!accessToken){
+        history.push('/')
+    }
 
     useEffect(()=>{
         api.get('book/request',{
@@ -36,15 +41,47 @@ function Dashboard(){
         }
     }
 
+    async function handleSwitchDropdown(event){
+        const dropdown = event.target.closest('div.dropdown')
+
+        if(dropdown.classList.contains('active')){
+            dropdown.classList.remove('active')
+        }
+        else{
+            dropdown.classList.add('active')
+        }
+    }
+
+    async function handleLogout(){
+        localStorage.clear()
+        history.push('/')
+    }
+
     return (
         <div className="DashBoard">
             <header>
                 <h1 className="projectTitle"><img src={logo} alt=""/><p>Book<span>Sharing</span></p></h1>
                 <div className="menu">
+                    <div className="dropdown">
+                        <button className="AddNew" onClick={handleSwitchDropdown}><FiPlus color="white" size="25px"/></button>
+                        <div className="dropdown-content" onMouseLeave={handleSwitchDropdown}>
+                            <ul>
+                                <li><button><FiPlus color="#ccc" size="20px"/> <p> Solicitação</p></button></li>
+                                <li><button><FiPlus color="#ccc" size="20px"/> <p> Compartilhamento</p></button></li>
+                            </ul>
+                        </div>
+                    </div>
                     <div className="profilePic">
                         <img src="./profile.png" alt=""/>
                     </div>
-                    <button><FiSettings color="white" size="25px"/></button>
+                    <div className="dropdown">
+                        <button className="settings" onClick={handleSwitchDropdown}><FiSettings color="white" size="25px"/></button>
+                        <div className="dropdown-content" onMouseLeave={handleSwitchDropdown}>
+                            <ul>
+                                <li><button onClick={handleLogout}><FiLogOut color="#ccc" size="20px"/> Sair</button></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </header>
             <div className="dash-itens">
